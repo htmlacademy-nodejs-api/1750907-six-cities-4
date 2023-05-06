55bb6984eba7efaf309f9a0d18b20caf24eafa15
Dmitry Khalimov <dmitry.khalimov@gmail.com>
2023-05-06 22:41:22 +0300

    Добавляет команду импорта

--- src/core/cli-command/import.command.ts
+++ src/core/cli-command/import.command.ts
@@ -1,8 +1,19 @@
+import TSVFileReader from '../file-reader/tsv-file-reader.js';
 import { CliCommandInterface } from './cli-command.interface.js';
 
 export default class ImportCommand implements CliCommandInterface {
   public readonly name = '--import';
-  public execute(filenames: string): void {
-    //
+  public execute(filename: string): void {
+    const fileReader = new TSVFileReader(filename.trim());
+
+    try {
+      fileReader.read();
+      console.log(fileReader.toArray());
+    } catch (err) {
+      if (!(err instanceof Error)) {
+        throw err;
+      }
+      console.log(`Не удалось импортировать данные из файла по причине: "${err.message}"`);
+    }
   }
 }
