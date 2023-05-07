b4da40f297cc0029616054f882385ab99e5a8697
Dmitry Khalimov <dmitry.khalimov@gmail.com>
2023-05-07 16:54:08 +0300

    Добавляет генератор данных

--- src/core/cli-command/generate-command.ts
+++ src/core/cli-command/generate-command.ts
@@ -1,13 +1,27 @@
+import got from 'got';
+import OfferGenerator from '../../modules/offer-generator/offer-generator.js';
 import { MockData } from '../../types/mock-data.type.js';
-import TSVFileReader from '../file-reader/tsv-file-reader.js';
 import { CliCommandInterface } from './cli-command.interface.js';
+import { appendFile } from 'node:fs/promises';
 
 export default class GenerateCommand implements CliCommandInterface {
   public readonly name = '--generate';
-  private initalData!: MockData;
+  private initialData!: MockData;
 
   public async execute(...parameters:string[]): Promise<void> {
     const [count, filepath, url] = parameters;
     const offerCount = Number.parseInt(count, 10);
+
+    try {
+      this.initialData = await got.get(url).json();
+    } catch {
+      console.log(`Can't fetch data from ${url}.`);
+    }
+
+    const offerGeneratorString = new OfferGenerator(this.initialData);
+
+    for (let i = 0; i < offerCount; i++) {
+      await appendFile(filepath, `${offerGeneratorString.generate()}\n`, 'utf8');
+    }
   }
 }
