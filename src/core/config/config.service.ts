7045c511a7d03aebe796ed491f8cd2ecebfdd3a3
Dmitry Khalimov <dmitry.khalimov@gmail.com>
2023-05-13 16:18:37 +0300

    Подключает dotnet.env к конфигу

--- src/core/config/config.service.ts
+++ src/core/config/config.service.ts
@@ -1,9 +1,22 @@
+import { LoggerInterface } from '../logger/logger.interface.js';
 import { ConfigInterface } from './config.interface.js';
+import { DotenvParseOutput, config } from 'dotenv';
 
 export default class ConfigService implements ConfigInterface {
+  private readonly config: NodeJS.ProcessEnv;
+
   constructor (
-    private readonly config: NodeJS.ProcessEnv
-  ) {}
+    private readonly logger: LoggerInterface,
+  ) {
+    const parsedOutput = config();
+
+    if (parsedOutput.error) {
+      throw new Error('Can\'t read .env file.');
+    }
+
+    this.config = parsedOutput.parsed as DotenvParseOutput;
+    this.logger.info('.env file found and successfully parsed!');
+  }
 
   public get(key: string): string | undefined {
     return this.config[key];
