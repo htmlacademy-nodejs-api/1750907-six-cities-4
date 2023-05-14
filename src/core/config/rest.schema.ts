8c3ed849b33dc3678edf829d3b6b2b67621436c9
Dmitry Khalimov <dmitry.khalimov@gmail.com>
2023-05-14 12:09:39 +0300

    Расширяет схему rest

--- src/core/config/rest.schema.ts
+++ src/core/config/rest.schema.ts
@@ -1,7 +1,9 @@
 import convict from 'convict';
 
 export type RestSchema = {
-  PORT: number
+  PORT: number;
+  SALT: string;
+  DB_HOST: string;
 }
 
 export const configRestSchema = convict<RestSchema>({
@@ -10,5 +12,17 @@ export const configRestSchema = convict<RestSchema>({
     format: 'port',
     env: 'PORT',
     default: 4000
+  },
+  SALT: {
+    doc: 'Salt for password hash',
+    format: String,
+    env: 'SALT',
+    default: null,
+  },
+  DB_HOST: {
+    doc: 'IP address of the database server (MongoDB)',
+    format: 'ipaddress',
+    env: 'DB_HOST',
+    default: '127.0.0.1'
   }
 });
