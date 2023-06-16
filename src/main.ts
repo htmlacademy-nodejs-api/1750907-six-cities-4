47ed19a99fd3ae8a6f78a94401d7bf75e22a41bf
Dmitry Khalimov <dmitry.khalimov@gmail.com>
2023-06-16 20:44:11 +0300

    Добавляет injectable Mongo

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,9 @@ import PinoService from './core/logger/pino.service.js'; // логгер
 import { AppComponent } from './types/app-component.enum.js';
 import { LoggerInterface } from './core/logger/logger.interface.js';
 import { ConfigInterface } from './core/config/config.interface.js';
+import { DatabaseClientInterface } from './core/database-client/database-client.interface.js';
 import { RestSchema } from './core/config/rest.schema.js';
+import MongoClientService from './core/database-client/mongo-client.service.js';
 
 async function bootstrap() {
   const container = new Container();
@@ -16,6 +18,7 @@ async function bootstrap() {
   container.bind<RestApplication>(AppComponent.RestApplication).to(RestApplication).inSingletonScope();
   container.bind<LoggerInterface>(AppComponent.LoggerInterface).to(PinoService).inSingletonScope();
   container.bind<ConfigInterface<RestSchema>>(AppComponent.ConfigInterface).to(ConfigService).inSingletonScope();
+  container.bind<DatabaseClientInterface>(AppComponent.DatabaseClientInterface).to(MongoClientService).inSingletonScope();
 
   const application = container.get<RestApplication>(AppComponent.RestApplication);
   await application.init();
