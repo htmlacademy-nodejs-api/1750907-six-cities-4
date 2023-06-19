76abff106bd99a9db6073256b0ea022b59cb8f26
Dmitry Khalimov <dmitry.khalimov@gmail.com>
2023-06-19 18:20:01 +0300

    Добавляет создание модели

--- src/modules/author/author.entity.ts
+++ src/modules/author/author.entity.ts
@@ -1,8 +1,20 @@
 import { Author } from '../../types/author.type.js';
+import typegoose, { getModelForClass } from '@typegoose/typegoose';
+
+const { prop } = typegoose;
 
 export class AuthorEntity implements Author {
+  @prop({ unique: true, required: true })
   public email = '';
+
+  @prop({ required: false, default: ''})
   public avatarPath = '';
+
+  @prop({required: true})
   public firstname = '';
+
+  @prop({required: true})
   public lastname = '';
 }
+
+export const AuthorModel = getModelForClass(AuthorEntity);
