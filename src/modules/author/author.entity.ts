e8a78df58186f4b337e883c95dbf334dc471e284
Dmitry Khalimov <dmitry.khalimov@gmail.com>
2023-06-19 18:57:31 +0300

    Добавляет возможность задания пароля

--- src/modules/author/author.entity.ts
+++ src/modules/author/author.entity.ts
@@ -1,9 +1,17 @@
 import { Author } from '../../types/author.type.js';
 import typegoose, { defaultClasses, getModelForClass } from '@typegoose/typegoose';
+import { createSHA256 } from '../../core/helpers/common.js';
 
-const { prop } = typegoose;
+const { prop, modelOptions } = typegoose;
 
 export interface AuthorEntity extends defaultClasses.Base{}
+
+@modelOptions({
+  schemaOptions: {
+    collection: 'authors'
+  }
+})
+
 export class AuthorEntity extends defaultClasses.TimeStamps implements Author {
   @prop({ unique: true, required: true })
   public email = '';
@@ -11,11 +19,31 @@ export class AuthorEntity extends defaultClasses.TimeStamps implements Author {
   @prop({ required: false, default: ''})
   public avatarPath = '';
 
-  @prop({required: true})
+  @prop({required: true, default: ''})
   public firstname = '';
 
-  @prop({required: true})
+  @prop({required: true, default: ''})
   public lastname = '';
+
+  @prop({required: true, default: ''})
+  private password?: string;
+
+  constructor(authorData: Author) {
+    super();
+
+    this.email = authorData.email;
+    this.avatarPath = authorData.avatarPath;
+    this.firstname = authorData.firstname;
+    this.lastname = authorData.lastname;
+  }
+
+  public setPassword(password: string, salt: string) {
+    this.password = createSHA256(password, salt)
+  }
+
+  public getPassword() {
+    return this.password;
+  }
 }
 
 export const AuthorModel = getModelForClass(AuthorEntity);
